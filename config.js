55f633f68f3691f0b7f849965726165349e76bb6
DominicDev <s0l0estudi4r@gmail.com>
2025-12-27 10:12:50 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'MAZEMIRROR';
+export const nameSongs = [
+	"GLARING YOUTH", /*1) (8.6/10)*/
+	"EMPIRE", /*2*/
+	"ROBBED", /*3*/
+	"LIVING", /*4*/
+	"PARTNER IN CRIME", /*5*/
+	"ELLE", /*6*/
+	"THE NIGHT", /*7*/
+	"THE POOL", /*8*/
+	"SIGNS", /*9*/
+	"COMPLY", /*10*/
+	"QUEEN", /*11*/
+	"FEEL", /*12*/
+	"NGBA", /*13*/
+	"STARS`N`STONES", /*14*/
+	"SELFISH", /*15*/
+	"GHOST", /*16*/
+	"GO LIGHT", /*17*/
+	"GLARING YOUTH (WALT REMIX)", /*18*/
+	"GO LIGHT (INSTRUMENTAL)", /*19*/
+	"SIGNS FT AREND", /*20*/
+]
